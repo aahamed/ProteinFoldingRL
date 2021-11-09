10950cb36fc0cc427a40b8db2a8d28152b404ec9
Jay Rainton <jayrainton@gmail.com>
2021-11-08 16:01:16 -0800

    debug in special pull

--- pulling_env/pulling_env.py
+++ pulling_env/pulling_env.py
@@ -512,10 +512,7 @@ class Pulling2DEnv(gym.Env):
         cn_y, cn_x = current_node
         nn_y, nn_x = next_node
 
-        print(current_node, next_node)
-
         if (cn_x == nn_x and (cn_y-1 == nn_y or cn_y+1 == nn_y)) or (cn_y == nn_y and (cn_x-1 == nn_x or cn_x+1 == nn_x)):
-            print("Entered here")
             return False
 
         return True

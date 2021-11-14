6a17d963d4f28614132ac000629788256c6a0aed
Jay Rainton <jayrainton@gmail.com>
2021-11-13 17:05:11 -0800

    testing

--- pulling_env/A2C_agent.py
+++ pulling_env/A2C_agent.py
@@ -9,7 +9,7 @@ import time
 # Create environment
 # env = gym.make('LunarLander-v2')
 seq = 'hhppppphhppphppphp' # Our input sequence
-seq = 'HHPPHHHH' # Our input sequence
+seq = 'HHPPHH' # Our input sequence
 seq = seq.upper()
 env = Pulling2DEnv(seq, collision_penalty=-.01)
 

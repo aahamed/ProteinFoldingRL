f3c57d8f8bb0458b7f0c7f5fd7525bfcbf583bc4
Jay Rainton <jayrainton@gmail.com>
2021-11-16 20:27:00 -0800

    trying profolding

--- pulling_env/A2C_agent.py
+++ pulling_env/A2C_agent.py
@@ -10,7 +10,7 @@ import time
 # Create environment
 # env = gym.make('LunarLander-v2')
 seq = 'hhppppphhppphppphp' # Our input sequence
-seq = 'HHPPHHPPHH' # Our input sequence
+#seq = 'HHPPHHPPHH' # Our input sequence
 seq = seq.upper()
 env = Pulling2DEnv(seq, collision_penalty=-.01)
 

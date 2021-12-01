85ffe669281cf3610e159123f170056143714f6e
Jay Rainton <jayrainton@gmail.com>
2021-12-01 01:32:22 -0800

    updating

--- pulling_env/A2C_agent.py
+++ pulling_env/A2C_agent.py
@@ -6,44 +6,56 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3 import PPO
 import time
 
+import numpy as np
+
 
 # Create environment
 # env = gym.make('LunarLander-v2')
 seq = 'phpphphhhphhphhhhh' # Our input sequence
-seq = 'HHPPHHHH' # Our input sequence
+seq = 'HHPPHH' # Our input sequence
 seq = seq.upper()
 env = Pulling2DEnv(seq, collision_penalty=-.01)
 
 # Instantiate the agent
 #model = A2C("MlpPolicy", env, verbose=1, tensorboard_log='./tensorboard')
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log='./tensorboard')
-start = time.time()
-model.learn(total_timesteps=int(2e5))
-end = time.time()
-# Save the agent
-model.save("A2C_pulling")
-del model  # delete trained model to demonstrate loading
 
-# Load the trained agent
-# NOTE: if you have loading issue, you can pass `print_system_info=True`
-# to compare the system on which the model was trained vs the current one
-# model = DQN.load("dqn_lunar", env=env, print_system_info=True)
-model = A2C.load("A2C_pulling", env=env)
+total_arr = []
 
-# Evaluate the agent
-# NOTE: If you use wrappers with your environment that modify rewards,
-#       this will be reflected here. To evaluate with original rewards,
-#       wrap environment in a "Monitor" wrapper before other wrappers.
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+for i in range(5):
+	model = PPO("MlpPolicy", env, verbose=1, tensorboard_log='./tensorboard')
+	start = time.time()
+	model.learn(total_timesteps=int(2e5))
+	end = time.time()
+	# Save the agent
+	model.save("A2C_pulling")
+	del model  # delete trained model to demonstrate loading
 
-# Enjoy trained agent
-obs = env.reset()
-env.render()
-i = 0
-while not env.done or i > 1000:
-	action, _states = model.predict(obs, deterministic=True)
-	obs, rewards, dones, info = env.step( action )
+	# Load the trained agent
+	# NOTE: if you have loading issue, you can pass `print_system_info=True`
+	# to compare the system on which the model was trained vs the current one
+	# model = DQN.load("dqn_lunar", env=env, print_system_info=True)
+	model = A2C.load("A2C_pulling", env=env)
+
+	# Evaluate the agent
+	# NOTE: If you use wrappers with your environment that modify rewards,
+	#       this will be reflected here. To evaluate with original rewards,
+	#       wrap environment in a "Monitor" wrapper before other wrappers.
+	mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+
+	# Enjoy trained agent
+	obs = env.reset()
 	env.render()
-	i += 1
+	i = 0
+	while not env.done or i > 1000:
+		action, _states = model.predict(obs, deterministic=True)
+		obs, rewards, dones, info = env.step( action )
+		env.render()
+		i += 1
 
-print(f"Total time needed to train: {end-start}")
+	print(f"Total time needed to train: {end-start}")
+	total_arr.append(end-start)
+
+with open('HHPPHH.npy', 'wb') as f:
+	np.save(f, np.asarray(total_arr))
+
+with open ('HHPPHH.csv', 'a+')
